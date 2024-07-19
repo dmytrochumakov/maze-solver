5984bc3703e93a86b943e5239289dc88749ac1bd
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2024-07-19 13:43:40 +0300

    added Point, and Line classes

--- graphics.py
+++ graphics.py
@@ -11,7 +11,7 @@ class Window:
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
     def close(self):
-    self.__running = False
+        self.__running = False
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -22,3 +22,24 @@ class Window:
         while self.__running:
             self.redraw()
         print("window closed...")
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def init(self, x, y):
+        self.x = x 
+        self.y = y
+
+
+class Line:
+    def init(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2        
+        
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x1, self.p1.y1, self.p2.x2, self.p2.y2, fill=fill_color, width=2
+        )
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from graphics import Window
 
 def main():
     win = Window(800, 600)
+    l = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l, "black")
     win.wait_for_close()
 
 main()
